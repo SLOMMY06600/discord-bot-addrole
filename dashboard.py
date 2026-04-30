98821a2c8b0e663a068ba648e73b95a6d6088cd4
SLOMMY06600 <dubmax20@gmail.com>
2026-04-30 19:10:42 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,7 +3,7 @@ from flask_socketio import SocketIO
 import os
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode="threading")
+socketio.run(app, host="127.0.0.1", port=5000, debug=True)
 
 logs = []
 
